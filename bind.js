fe58557a6bd8b5ceadddeab725b24fe546e2f172
Asaduzzaman Sunam <asaduzzaman.sunam@gmail.com>
2022-08-05 20:28:17 +0600

    borrow a method from another object using bind

---
+++ bind.js
@@ -0,0 +1,21 @@
+const masud = {
+  name: "Masud Kibria",
+  id: 120,
+  balance: 5000,
+  treat: function (expense) {
+    this.balance = this.balance - expense;
+    console.log("called from here", this);
+    return this.balance;
+  },
+};
+
+const hero = {
+  name: "Hero Alam",
+  id: 101,
+  balance: 6000,
+};
+
+// masud.treat(500);
+
+const heroTreat = masud.treat.bind(hero);
+console.log(heroTreat(500));
